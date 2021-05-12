bc0386160268fd8b787529fb88a6ef732d87b475
123JackCole <123jack.cole@gmail.com>
2021-05-11 17:32:59 -0700

    experimenting with rows and columns

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./styles/App.css";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 
 // import NavBar from "./components/NavBar.js";
 import Name from "./components/Name.js";
+import About from "./containers/About";
+import Projects from "./containers/Projects.js";
+import Contact from "./containers/Contact.js";
+import Pets from "./containers/Pets.js";
 
 const App = () => {
   return (
     <>
-      <Container fluid>
-        <Row>
-          <Col>
-            <div>
-              <Name />
-            </div>
-          </Col>
-        </Row>
+      <Container>
+        <Name />
+        <About />
+        <Projects />
+        <Contact />
+        <Pets />
       </Container>
     </>
   );
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Name = () => {
   return (
     <>
-      <h2> Jack Cole </h2>
-      <p> Full Stack Software Engineer </p>
+      <Row>
+        <Col>
+          <div id="banner">
+            <div className="inner">
+              <h2> Jack Cole </h2>
+              <p> Full Stack Software Engineer </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </>
   );
 };
